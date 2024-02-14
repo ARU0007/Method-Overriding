63f72efd9a55c7e22069fd027aaa5f0c870eda8a
ARU0007 <aruneshagrahari95@gmail.com>
2024-02-14 15:45:35 +0530

    create a class(Thor) and override the method(displayInfo()).

---
+++ src/thor/Thor.java
@@ -0,0 +1,10 @@
+package thor;
+
+import marvel.Marvel;
+
+public class Thor extends Marvel {
+    @Override
+    public void displayInfo() {
+        System.out.println("God Of Thunder");       //override
+    }
+}
